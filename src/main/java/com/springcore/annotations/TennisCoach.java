46ef1c3973d9bb81f1b5339f15500887448d2f7f
Deepak Verma <deepakverma.pb34@gmail.com>
2023-09-10 23:29:57 +0530

    annotations - constructor injection

--- src/main/java/com/springcore/annotations/TennisCoach.java
+++ src/main/java/com/springcore/annotations/TennisCoach.java
@@ -1,15 +1,22 @@
 package com.springcore.annotations;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class TennisCoach implements Coach {
 	
 	private FortuneService fortuneService;
+	
+	public TennisCoach() {
+		System.out.println("Tennis Coach: Default constructor!!");
+	}
 	
 	@Autowired
 	public TennisCoach(FortuneService fortuneService) {
 		super();
 		this.fortuneService = fortuneService;
+		System.out.println("Tennis Coach: Parameterized constructor Injection!");
 	}
 
 	public String getDailyWorkout() {
@@ -19,4 +26,11 @@ public class TennisCoach implements Coach {
 	public String getDailyFortune() {
 		return fortuneService.getFortune();
 	}
+	
+//	@Autowired
+	public void setFortuneService(FortuneService fortuneService) {
+		this.fortuneService = fortuneService;
+		System.out.println("Setter Injection!");
+	}
+
 }
